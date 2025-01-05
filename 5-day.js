eddd714f2f0de250df1e6785b10d44f0cfd8b3c8
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-01-05 19:54:17 +0500

    5-day's leetcode problem solved

---
+++ 5-day.js
@@ -0,0 +1,7 @@
+var theMaximumAchievableX = function (num, t) {
+  return num + 2 * t;
+};
+
+theMaximumAchievableX(3, 2); // 7
+
+// https://leetcode.com/problems/find-the-maximum-achievable-number/
