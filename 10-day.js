e2003cf84f2658e3b40d835f5cea47103777703f
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-01-10 17:04:56 +0500

    10-day solving leetcode problems

---
+++ 10-day.js
@@ -0,0 +1,9 @@
+var scoreOfString = function (s) {
+  let sum = 0;
+  for (let i = 0; i < s.length - 1; i++) {
+    sum += Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1));
+  }
+  return sum;
+};
+
+console.log(scoreOfString("hello"));
