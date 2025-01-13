746dc621e0bd625123bf7913f9b59a18b4639664
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-01-13 20:32:41 +0500

    13-day solving leetcode problems

---
+++ 13-day.js
@@ -0,0 +1,7 @@
+var argumentsLength = function (...args) {
+  return args.length;
+};
+
+console.log(argumentsLength(1, 2, 3, 4));
+
+//https://leetcode.com/problems/return-length-of-arguments-passed
