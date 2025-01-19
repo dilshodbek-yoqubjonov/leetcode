5cfdcc5d25c618187f8d5a10f530d84aef704833
Yoqubjonov Dilshodbek <yoqubjonovdilshodo4@gmail.com>
2025-01-19 13:05:52 +0500

    15-day solving leetcode problems

---
+++ 15-day.js
@@ -0,0 +1,22 @@
+// var isSameAfterReversals = function (num) {
+//   num = num.toString();
+//   if (num.length === 1) {
+//     return true;
+//   } else return num[num.length - 1] !== "0";
+// };
+
+// console.log(isSameAfterReversals(602));
+
+//https://leetcode.com/problems/a-number-after-a-double-reversal?envType=problem-list-v2&envId=math
+
+// var minimumSum = function (num) {
+//   num = num.toString();
+//   num = num.split("").map(Number);
+//   num = num.sort();
+
+//   let ans = num[0] + num[3] + num[1] + num[2];
+//   return ans;
+// };
+
+// console.log(minimumSum(2932));
+// https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits
